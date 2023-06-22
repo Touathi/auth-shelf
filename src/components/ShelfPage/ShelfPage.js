fa5a7f988979d811dadf862454849a17ba39cc57
Sabina Gahramanova <sabina.ghrmnli@gmail.com>
2023-06-22 14:30:56 -0500

    maping through items

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import ShelfItem from '../ShelfItem/ShelfItem'
 
+
+//mapping through items in the shelf page
 function ShelfPage() {
+  const itemList = useSelector(store => store.itemReducer)
   return (
     <div className="container">
+
       <h2>Shelf</h2>
+
       <p>All of the available items can be seen here.</p>
+      <ul>
+        {itemList.map(item => (
+          <li key={item.id}>
+            <ShelfItem item = {item}/>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
